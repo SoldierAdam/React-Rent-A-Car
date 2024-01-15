8ad10b8aa231a45d97e5b1a35c9ff1832b1f68d0
bilaleknc <etkhckrone@gmail.com>
2024-01-15 16:06:21 +0300

    edited little things

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.css";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
