b0331938871ab216b06c4858f088c11cfa2d85aa
Erman İskender <45319030+SoldierAdam@users.noreply.github.com>
2024-01-16 21:56:41 +0300

    style changes and navbar changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
 	
@@ -10,24 +13,29 @@ function Navbar() {
   return (
 	<nav id="navbar">
 	<div className="container">
-	  <h1 className="logo"><Link to="/">HBT</Link></h1>
-	  <ul>
-		<li>
+	  <div className="logo"><Link to="/" className='logo'>CarRent</Link></div>
+	  <div className='menu'>
+		<div className='menu-link'>
       <NavLink to="/contact" className={location.pathname === "/contact" ? "current" : ""} >
         Contact
       </NavLink>
-	</li>
-	<li>
+	  	</div>
+		<div className='menu-link'>
 	  <NavLink to="/about" className={location.pathname === "/about" ? "current" : ""} >
 		About
       </NavLink>
-	</li>
-	<li>
+	  	</div>
+		<div className='menu-link'>
 	  <NavLink to="/cars" className={location.pathname === "/cars" ? "current" : ""} >
 		Cars
 	  </NavLink>
-    </li>
-	  </ul>
+	  	</div>
+		<div className='menu-link'>
+	  <NavLink to="/basket" className={location.pathname === "/basket" ? "current" : ""} >
+		<FontAwesomeIcon icon={faCartShopping as IconProp} />
+      </NavLink>
+	  </div>
+	  </div>
 	</div>
   </nav>
   )
