6c152a40a587452f48eb708635cf10105ff76197
Aysenur Sasmaz <aysenur_cmrt@hotmail.com>
2024-02-04 15:19:17 +0300

    login dropdown styling

--- src/components/Dropdown/DropdownList.tsx
+++ src/components/Dropdown/DropdownList.tsx
@@ -1,7 +1,6 @@
 import { NavDropdown } from 'react-bootstrap';
-import { NavLink, Link } from 'react-router-dom';
-import './DropdownList.css'
-
+import { NavLink } from 'react-router-dom';
+import './DropdownList.css';
 
 interface Props {
   user: any;
@@ -11,27 +10,21 @@ interface Props {
 const DropdownList = (props: Props) => {
   return (
     <div className="dropdown-container">
-   
-    <NavDropdown
-    id="nav-dropdown-dark-example"
-    title= {props.user.userName ? props.user.userName : "Login" }
-    menuVariant="dark"
-  >
-
-	{props.user.userName ? 
-	<>
-		<NavDropdown.Item as={NavLink} to="/profile">Profile</NavDropdown.Item> 
-		<NavDropdown.Item href="/">Logout</NavDropdown.Item>
-	</> :
-	<>
-		<NavDropdown.Item as={NavLink} to="/login">Login</NavDropdown.Item>
-		<NavDropdown.Item as={NavLink} to="/signUp">Sign Up</NavDropdown.Item>
-	</>
-	}
-
-	</NavDropdown>
+      <NavDropdown title={props.user.userName ? props.user.userName : "Login"} id="nav-dropdown-dark-example" menuVariant="dark">
+        {props.user.userName ? 
+          <>
+            <NavDropdown.Item as={NavLink} to="/profile">Profile</NavDropdown.Item> 
+            <NavDropdown.Item href="/">Logout</NavDropdown.Item>
+          </> :
+          <>
+            <NavDropdown.Item as={NavLink} to="/login" >Login</NavDropdown.Item>
+            <NavDropdown.Item as={NavLink} to="/signUp">Sign Up</NavDropdown.Item>
+          </>
+        }
+      </NavDropdown>
     </div>
   )
 }
 
-export default DropdownList
+export default DropdownList;
+
