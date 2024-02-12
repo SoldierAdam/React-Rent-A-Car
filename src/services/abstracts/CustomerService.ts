8f0a54b427aeeea48bf78c4d2c7bfccd8eaad637
Abdullah Cemil VANÇELİK <cemilvancelik@gmail.com>
2024-02-12 19:15:59 +0300

    added customer

---
+++ src/services/abstracts/CustomerService.ts
@@ -0,0 +1,24 @@
+import { AddCarRequest } from '../../models/cars/requests/addCarRequest';
+import { UpdateCarRequest } from '../../models/cars/requests/updateCarRequest';
+import { AddCarResponse } from '../../models/cars/responses/addCarResponse';
+import { GetAllCarResponse } from '../../models/cars/responses/getAllCarResponse';
+import { GetByIdCarResponse } from '../../models/cars/responses/getByIdCarResponse';
+import { UpdateCarResponse } from '../../models/cars/responses/updateCarResponse';
+import { BaseService } from "./baseService";
+
+
+class CustomerService extends BaseService <
+GetAllCarResponse,
+	GetByIdCarResponse,
+	AddCarRequest,
+	AddCarResponse,
+	UpdateCarRequest,
+	UpdateCarResponse
+    >
+{
+    constructor(){
+		super();
+		this.apiUrl = "customer";
+	}
+}
+export default new CustomerService();
