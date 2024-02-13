854fa2944c53a9eb7ea93d250d90a820894cfaad
Erman İskender <45319030+SoldierAdam@users.noreply.github.com>
2024-02-13 20:07:12 +0300

    new footer added, about us page updated

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,42 +1,83 @@
-import React from 'react';
-import "./../../styles/style.css";
+import "../About/About.css";
 
 const About = () => {
-  return (
-    <div>
-		<section id="about-info" className="bg-light py-3">
+	return (
+		<div>
 			<div className="container">
-			<div className="info-left">
-				<h1 className="l-heading"><span className="text-primary">About</span> Hotel BT</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem veritatis illo, similique labore voluptate nulla animi dolorum eius laborum illum, nesciunt quod reprehenderit dicta autem vel nobis minima sit deleniti!</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A velit voluptatem impedit voluptate. Doloribus, voluptas dolore! Cupiditate aliquid sequi deserunt.</p>
-			</div>
-			<div className="info-right">
-			<img src={process.env.PUBLIC_URL + "images/photo-1.jpg"} alt="Hotel BT" className="about-img" />
-			</div>
-			</div>
-		</section>
 
-		<div className="clr"></div>
-
-		<section id="testimonials" className="py-3">
-			<div className="container">
-			<h2 className="l-heading">What Our Guests Say</h2>
-			<div className="testimonial bg-primary">
-			<img src={process.env.PUBLIC_URL + "images/person-1.jpg"} alt="John Doe"/>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam eligendi quibusdam, dolorum maxime cum numquam quisquam, deleniti eum incidunt, velit non consectetur. Facere, ipsa maxime, ullam id amet odio laboriosam sit iusto tempore fugit exercitationem, a dolore quo maiores nisi!</p>
-			</div>
-
-			<div className="testimonial bg-primary">
-			<img src={process.env.PUBLIC_URL + '/images/person-2.jpg'} alt="Sara Williams" />
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam eligendi quibusdam, dolorum maxime cum numquam quisquam, deleniti eum incidunt, velit non consectetur. Facere, ipsa maxime, ullam id amet odio laboriosam sit iusto tempore fugit exercitationem, a dolore quo maiores nisi!</p>
+				<div className="about">
+					<div className="left">
+						<h1>About us</h1>
+						<hr />
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+					<div className="right">
+						<img src="" />
+					</div>
+					<div className="clear"></div>
 				</div>
+
+				<div className="mission">
+					<div className="left">
+						<img src="" />
+					</div>
+					<div className="right">
+						<h1>Mission Statement</h1>
+						<hr />
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+					<div className="clear"></div>
+				</div>
+
+				<div className="team">
+					<h1>Our Team<hr/></h1>
+					<div className="card">
+						<div className="circle-container">
+						</div>
+						<h2>Bilal Ekinci</h2>
+						<h4>Corporate Partner</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+
+					<div className="card">
+						<div className="circle-container">
+						</div>
+						<h2>Ayşenur Şaşmaz</h2>
+						<h4>Corporate Partner</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+
+					<div className="card">
+						<div className="circle-container">
+						</div>
+						<h2>Zeynep Ozan</h2>
+						<h4>Corporate Partner</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+
+					<div className="card">
+						<div className="circle-container">
+						</div>
+						<h2>Erman İskender</h2>
+						<h4>Corporate Partner</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+
+					<div className="card">
+						<div className="circle-container">
+						</div>
+						<h2>Abullah Cemil Vançelik</h2>
+						<h4>Corporate Partner</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+
+				</div>
+
 			</div>
-		</section>
-
-
-    </div>
-  );
+		</div>
+	);
 }
 
 export default About;
