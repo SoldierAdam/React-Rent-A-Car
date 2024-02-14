fae8e29eaef480c292e8a56363dd2095c48618fd
Bilal EKİNCİ <etkhckrone@gmail.com>
2024-02-14 22:11:01 +0300

    updated Payment details for invoice entity

--- src/pages/RentNow/RentNow.tsx
+++ src/pages/RentNow/RentNow.tsx
@@ -3,19 +3,22 @@ import CarDetails from './CarDetails';
 import SelectedCarInfo from './SelectedCarInfo';
 import { useState} from 'react';
 import PaymentDetails from "./PaymentDetails"
+import { useDispatch } from 'react-redux';
+import { setCustomerInfo } from '../../store/rentNow/rentSlice';
 
 
 
 export default function RentNow() {
 	const [showPayment, setShowPayment] = useState(false);
+	const dispatch = useDispatch();
 
 	const handleButtonClick = () => {
 	  setShowPayment(true);
-	  
 	};
 
 	const handleBackClick = () => {
 		setShowPayment(false);
+		dispatch(setCustomerInfo(null));
 	};
 
     return (
